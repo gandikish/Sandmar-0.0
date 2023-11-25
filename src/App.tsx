c344cc8ace946616e1fe0b9c9a353b7ef28a91bc
gandikish <mariavgg.2004@gmail.com>
2023-11-25 18:03:52 -0400

    principal page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 
 import * as React from 'react'
-import { Login } from './pages/Login.tsx'
+import Login from './pages/Login.tsx'
+import { Principal } from './pages/PrincipalPage.tsx'
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
 
 export const App = () => (
-  <Login/>
+  
+  <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/principal" element={<Principal />} />
+        </Routes>
+      </Router>
 )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -16,50 +16,57 @@ import {
   } from '@chakra-ui/react'
   import * as React from 'react'
   import { PasswordField } from '../components/PasswordField.tsx'
-  
-  export const Login = () => (
-    <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
-      <Stack spacing="8">
-        <Stack spacing="6">
-          <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
-            <Heading size={useBreakpointValue({ base: 'xl', md: 'xl' })}>
-              Inicia Sesión
-            </Heading>
-            <HStack spacing="1" justify="center">
-              <Text color="muted">No tienes cuenta?</Text>
-              <Button variant="link" colorScheme="green">
-                Registrate
-              </Button>
-            </HStack>
-          </Stack>
-        </Stack>
-        <Box
-          py={{ base: '0', sm: '8' }}
-          px={{ base: '4', sm: '10' }}
-          bg={useBreakpointValue({ base: 'transparent', sm: 'bg-surface' })}
-          boxShadow={{ base: 'none', sm: useColorModeValue('md', 'md-dark') }}
-          borderRadius={{ base: 'none', sm: 'xl' }}
-        >
+  import { useNavigate } from 'react-router-dom'
+
+
+    const Login = (props) => {
+        const navigate = useNavigate();
+      
+        return (
+        <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
+        <Stack spacing="8">
           <Stack spacing="6">
-            <Stack spacing="5">
-              <FormControl>
-                <FormLabel htmlFor="email">Correo</FormLabel>
-                <Input id="email" type="email" />
-              </FormControl>
-              <PasswordField />
-            </Stack>
-            <HStack justify="space-between">
-              <Checkbox defaultChecked colorScheme='green'>Recuerdame</Checkbox>
-              <Button variant="link" colorScheme="green" size="sm">
-                Olvidó su contraseña?
-              </Button>
-            </HStack>
-            <Stack spacing="6">
-              <Button colorScheme='green'>Ingresar</Button>
+            <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
+              <Heading size={useBreakpointValue({ base: 'xl', md: 'xl' })}>
+                Inicia Sesión
+              </Heading>
+              <HStack spacing="1" justify="center">
+                <Text color="muted">No tienes cuenta?</Text>
+                <Button variant="link" colorScheme="green">
+                  Registrate
+                </Button>
+              </HStack>
             </Stack>
           </Stack>
-        </Box>
-      </Stack>
-    </Container>
-  )
-  
+          <Box
+            py={{ base: '0', sm: '8' }}
+            px={{ base: '4', sm: '10' }}
+            bg={useBreakpointValue({ base: 'transparent', sm: 'bg-surface' })}
+            boxShadow={{ base: 'none', sm: useColorModeValue('md', 'md-dark') }}
+            borderRadius={{ base: 'none', sm: 'xl' }}
+          >
+            <Stack spacing="6">
+              <Stack spacing="5">
+                <FormControl>
+                  <FormLabel htmlFor="email">Correo</FormLabel>
+                  <Input id="email" type="email" />
+                </FormControl>
+                <PasswordField />
+              </Stack>
+              <HStack justify="space-between">
+                <Checkbox defaultChecked colorScheme='green'>Recuerdame</Checkbox>
+                <Button variant="link" colorScheme="green" size="sm">
+                  Olvidó su contraseña?
+                </Button>
+              </HStack>
+              <Stack spacing="6">
+                <Button colorScheme='green' onClick={() => navigate("/principal")}>Ingresar</Button>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Container>)
+      }
+    
+      export default Login
+
